199c1a7a78bff6daa07559d60b1551eb196ec3ac
Raymond Chen <ahrchen@gmail.com>
2022-02-04 15:10:08 -0800

    Initial Commit

---
+++ Conversion/ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionApp.swift
+//  Conversion
+//
+//  Created by Raymond Chen on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
